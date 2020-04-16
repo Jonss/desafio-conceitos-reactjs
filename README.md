20741b197f0dbf8b4873e7d23241a0a2478e5537
Joao Santana <joaosantana.ti@gmail.com>
2020-04-16 12:43:18 -0300

    Implementing integration with backend to add repository and remove repository

---
+++ README.md
@@ -0,0 +1,17 @@
+## desafio-conceitos-reactjs
+
+### Como executar?
+
+Na primeira vez que executar, é necessário instalar as dependências com o comando 
+`$ yarn install`.
+
+Após isso, execute o comando
+`$yarn start`
+
+### Como testar?
+
+Execute o comando `$yarn test`
+
+### Como usar? 
+
+Usando o [backend](https://github.com/Jonss/desafio-conceitos-reactjs), use os botões adicionar, ou remover para incluir repositórios.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+        setRepositories(response.data)
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Projeto ${Date.now()}`, 
+      url: `http://github.com/Jonss/project${Date.now()}`, 
+      techs: ["react", "Js", "nodeJs"]
+    });
+
+    const repo = response.data;
+    setRepositories([...repositories, repo]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`, () => {});
+    const repos = repositories.filter(repo => repo.id !== id);
+    setRepositories(repos);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repo => <li key={repo.id}>
+          {repo.title}
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
